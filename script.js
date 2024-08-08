e604dd8ebf5390c94ad2e647a49829bb63509781
abdielpeguero <abdielpeguerocastro@gmail.com>
2024-08-08 12:42:10 -0400

    Scroll btns ready
    added js functions for scroll btns

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ document.addEventListener('DOMContentLoaded', function () {
             .from('.portfolio_profile_pic', { duration: 1, opacity: 0, y: -50 })
             .from('.greetings', { duration: 1, opacity: 0, y: 50 }, '-=0.5')
             .from('.short_bio', { duration: 1, opacity: 0, y: 50 }, '-=0.5')
-            .from('.primary_button', { duration: 1, opacity: 0, y: 50 }, '-=0.5');
 
         // Mark that the hero animation has been played
         localStorage.setItem('heroAnimationPlayed', 'true');
@@ -58,5 +57,63 @@ document.addEventListener('DOMContentLoaded', function () {
     workTogetherTimeline
         .from('.available_for_work', { duration: 1, opacity: 0, x: -50 })
         .from('.get_your_project_running', { duration: 1, opacity: 0, x: 50 }, '-=0.5')
-        .from('.primary_button', { duration: 1, opacity: 0, y: 50 }, '-=0.5');
+});
+
+// Navbar navigation settings
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the navbar items
+    const navHome = document.getElementById('nav_home');
+    const navProjects = document.getElementById('nav_projects');
+    const navAbout = document.getElementById('nav_about');
+
+    // Add event listeners for each nav item
+    navHome.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default anchor behavior
+        navigateToPage('home.html');
+    });
+
+    navProjects.addEventListener('click', function(event) {
+        event.preventDefault();
+        navigateToPage('projects.html');
+    });
+
+    navAbout.addEventListener('click', function(event) {
+        event.preventDefault();
+        navigateToPage('about.html');
+    });
+
+    // Function to navigate to the specified page
+    function navigateToPage(page) {
+        window.location.href = page; // Navigate to the specified page
+    }
+});
+
+
+// Get the scroll up arrow element
+const scrollUpArrow = document.querySelector('.scroll_up_arrow_container');
+
+// Add a click event listener to the scroll up arrow
+scrollUpArrow.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+
+    // Smooth scroll to the top
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+// Get the scroll down arrow element
+const scrollDownArrow = document.querySelector('.scroll_down_arrow_container');
+
+// Add a click event listener to the scroll down arrow
+scrollDownArrow.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+
+    // Smooth scroll to my project section
+    window.scrollTo({
+        top: '1800',
+        behavior: 'smooth'
+    });
 });
